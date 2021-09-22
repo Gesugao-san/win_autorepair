12f9de77f0eb8da62fe220f1af4edb8f5def5f68
Gesugao-san <gesugao.san@gmail.com>
2021-09-22 18:17:20 +0300

    CHKDSK updated

--- win_autorepair.cmd
+++ win_autorepair.cmd
@@ -29,7 +29,26 @@ DISM /Online /Cleanup-image /RestoreHealth
 
 @REM CHKDSK
 ECHO ***
-CHKDSK "%SystemDrive%" /F /R /offlinescanandfix
+
+@REM CHKDSK "%SystemDrive%" /F /R /offlinescanandfix
+
+
+ENDLOCAL && SETLOCAL ENABLEDELAYEDEXPANSION
+@REM https://serverfault.com/a/268814
+SET /a count=1
+SET "delimiter=, "
+SET "labels_free=Z:\ Y:\ X:\ W:\ V:\ U:\ T:\ S:\ R:\ Q:\ P:\ O:\ N:\ M:\ L:\ K:\ J:\ I:\ H:\ G:\ F:\ E:\ D:\ C:\ B:\ A:\ "
+FOR /F "skip=1 tokens=1 delims= " %%a IN ('wmic logicaldisk get caption') DO (
+    @REM Sanitizing newline and non-labels
+    @REM https://newbedev.com/how-to-see-if-a-string-contains-a-substring-using-batch
+    ECHO [%count%] Detected label "%%a", repairing | FIND ":"
+    SET /a count=%count%+1
+    SET "labels_used=!labels_used!%%a\%delimiter%"
+    SET "labels_free=!labels_free:%%a =!"
+    CHKDSK "%SystemDrive%" /F /R /offlinescanandfix
+)
+SET labels_used=%labels_used:~0,-8%
+SET labels
 
 ECHO ******************
 ECHO Done working.
