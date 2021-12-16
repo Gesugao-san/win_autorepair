602f1906ba3b7f73b4d3f6dd701c8eaf37a79460
Gesugao-san <gesugao.san@gmail.com>
2021-12-16 12:07:34 +0300

    Update win_autorepair.cmd

--- win_autorepair.cmd
+++ win_autorepair.cmd
@@ -2,30 +2,32 @@
 @ECHO OFF
 CLS
 SETLOCAL ENABLEDELAYEDEXPANSION DISABLEDELAYEDEXPANSION
+SET root_disk=%~d0
 SET root_path=%~dp0
 
 ECHO This script will help you with OS repairing.
 
 ECHO Start working...
 CHCP
+ECHO PATH: %PATH%
 CD /D "%root_path%"
 ECHO ******************
 
-@REM SFC
+ECHO ***
+@REM Windows Recovery Environment (WinRE) info
+reagentc.exe /info
+ECHO ***
+reagentc.exe /enable
+
+@REM SFC (part 1)
 SFC /?
 ECHO ***
-SFC /SCANNOW
+SFC.exe /VERIFYONLY /OFFLOGFILE="%root_disk%:\SFC.log"
+ECHO ***
+SFC.exe /SCANNOW /OFFLOGFILE="%root_disk%:\SFC.log"
+ECHO ***
+NOTEPAD.exe %WINDIR%\Logs\CBS\CBS.log
 
-@REM DISM
-ECHO ***
-@REM For PowerShell: Repair-WindowsImage -Online -RestoreHealth
-DISM /?
-ECHO ***
-DISM /Online /Cleanup-Image /CheckHealth
-ECHO ***
-DISM /Online /Cleanup-Image /ScanHealth
-ECHO ***
-DISM /Online /Cleanup-Image /RestoreHealth
 
 @REM CHKDSK
 ECHO ***
@@ -34,24 +36,33 @@ ENDLOCAL && SETLOCAL ENABLEDELAYEDEXPANSION
 @REM https://serverfault.com/a/268814
 SET /a count=1
 SET "delimiter=, "
-SET "labels_free=Z:\ Y:\ X:\ W:\ V:\ U:\ T:\ S:\ R:\ Q:\ P:\ O:\ N:\ M:\ L:\ K:\ J:\ I:\ H:\ G:\ F:\ E:\ D:\ C:\ B:\ A:\ "
-FOR /F "skip=1 tokens=1 delims= " %%a IN ('wmic logicaldisk get caption') DO (
+SET "labels_free=Z Y X W V U T S R Q P O N M L K J I H G F E D C B A "
+FOR /F "skip=1 tokens=1 delims= " %%a IN ('wmic.exe logicaldisk get caption') DO (
     @REM Sanitizing newline and non-labels
     @REM https://newbedev.com/how-to-see-if-a-string-contains-a-substring-using-batch
     ECHO [%count%] Detected label "%%a", repairing | FIND ":"
     SET /a count=%count%+1
     SET "labels_used=!labels_used!%%a\%delimiter%"
     SET "labels_free=!labels_free:%%a =!"
-    CHKDSK "%SystemDrive%" /F /R /offlinescanandfix
+    CHKDSK.exe "%SystemDrive%:\" /F /R /offlinescanandfix
+    
+    @REM SFC (part 1)
+    SFC.exe /SCANFILE="%SystemDrive%:\windows\system32\kernel32.dll" /OFFBOOTDIR="%SystemDrive%:\" /OFFWINDIR="%SystemDrive%:\windows\" /OFFLOGFILE="%root_disk%:\SFC.log"
+    SFC.exe /SCANFILE="%SystemDrive%:\windows\system32\winload.efi" /OFFBOOTDIR="%SystemDrive%:\" /OFFWINDIR="%SystemDrive%:\windows\" /OFFLOGFILE="%root_disk%:\SFC.log"
+    
+    @REM DISM
+    ECHO ***
+    @REM For PowerShell: Repair-WindowsImage -Online -RestoreHealth
+    DISM.exe /?
+    ECHO ***
+    DISM.exe /Online /Cleanup-Image /CheckHealth /LogLevel:4 /ScratchDir:"%root_disk%:\" /Image:"%SystemDrive%:\" /LogPath:"%root_disk%:\DISM_Check.log"
+    ECHO ***
+    DISM.exe /Online /Cleanup-Image /ScanHealth /LogLevel:4 /ScratchDir:"%root_disk%:\" /Image:"%SystemDrive%:\" /LogPath:"%root_disk%:\DISM_Scan.log"
+    ECHO ***
+    DISM.exe /Online /Cleanup-Image /RestoreHealth /LogLevel:4 /ScratchDir:"%root_disk%:\" /Image:"%SystemDrive%:\" /LogPath:"%root_disk%:\DISM_Restore.log"
 )
 SET labels_used=%labels_used:~0,-8%
 SET labels
-
-ECHO ***
-@REM Windows Recovery Environment (WinRE) info
-reagentc /info
-ECHO ***
-reagentc /enable
 
 ECHO ***
 @REM BootRec (WinRE only!)
